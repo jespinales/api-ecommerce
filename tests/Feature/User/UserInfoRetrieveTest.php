d736ca439ad98da2df8bdfa621a75de43e7ce064
jespinales <juliocesarespinales@gmail.com>
2023-06-17 21:35:54 -0500

    Added UserInfoRetrieveTest

---
+++ tests/Feature/User/UserInfoRetrieveTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\User;
+
+use App\Models\Client;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserInfoRetrieveTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function shouldRetrieveUserInfo(): void
+    {
+        $email = 'client@example.com';
+        $client = Client::factory()->create([
+            "email" => $email,
+            "password" => 'password'
+        ]);
+
+        $accessToken = $client->createToken('API token')->plainTextToken;
+
+        $response = $this->withHeaders([
+            'Authorization' => "Bearer $accessToken"
+        ])->get(route('api.client.me', ['client' => $client->id]));
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'status',
+            'data' => ['client']
+        ]);
+
+        $this->assertContains($email, $response->json('data.client'));
+    }
+}
